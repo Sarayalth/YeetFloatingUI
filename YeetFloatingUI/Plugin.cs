9e5b36512a22ee4dc0317ae57addc041c0b053eb
Saray <sarayalth@gmail.com>
2020-04-14 05:36:17 +0200

    Add project files.

---
+++ YeetFloatingUI/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+using HarmonyLib;
+namespace YeetFloatingUI
+{
+	[Plugin(RuntimeOptions.SingleStartInit)]
+	public class Plugin
+	{
+		internal static Plugin instance { get; private set; }
+		internal static string Name => "YeetFloatingUI";
+		internal static IPALogger log { get; set; }
+		[Init]
+		public void Init(IPALogger logger)
+		{
+			instance = this;
+			log = logger;
+		}
+		[OnStart]
+		public void OnApplicationStart()
+		{
+			log.Info("YEETING THAT UGLY THING.");
+			Harmony harmony = new Harmony("com.Sarayalth.FuckNewUI");
+			harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+		}
+		[OnExit]
+		public void OnApplicationQuit(){}
+	}
+}
---
+++ YeetFloatingUI/YeetRotationPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+namespace YeetFloatingUI
+{
+	[HarmonyPatch(typeof(BeatmapData))]
+	[HarmonyPatch("spawnRotationEventsCount", MethodType.Getter)]
+	class Yeet
+	{
+		static void Postfix(ref int __result)
+		{
+			if (__result > 0)
+				__result = 0;
+		}
+	}
+}
